66ffcf994393fab06bce0f4b62e0f1288e1f46c0
Alexander <alexsmg95@gmail.com>
2020-10-22 18:39:56 +0700

    1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -15,21 +15,24 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class main {
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
 
-        File file = new File("C:\\Users\\sav\\Music\\a.mp3");
+//        File file = new File("C:\\Users\\sav\\Music\\a.mp3");
+//
+//        AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+//        Map<?, ?> properties = ((TAudioFileFormat) fileFormat).properties();
+//        String key = "duration";
+//        Long microseconds = (Long) properties.get(key);
+//        System.out.println(microseconds / 1000000);
 
-        AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
-        Map<?, ?> properties = ((TAudioFileFormat) fileFormat).properties();
-        String key = "duration";
-        Long microseconds = (Long) properties.get(key);
-        System.out.println(microseconds / 1000000);
+        System.out.println(System.getenv("ffmpeg"));
 
-        ProcessBuilder processBuilder = new ProcessBuilder( "ffmpeg -i", file.toString(), file.toString().replaceAll("mp3", "flac"));
-        processBuilder.start();
-        File file2 = new File(file.toString().replaceAll("mp3", "flac"));
-        AudioSource source = new AudioSource.AudioFileSource(file2);
-        Fingerprinter fingerprinter = new Fingerprinter();
-        Fingerprint fingerprint = fingerprinter.apply(source).unsafeRunSync();
-        System.out.println(fingerprint.compressed());
-        file2.delete();
+
+//        ProcessBuilder processBuilder = new ProcessBuilder( "ffmpeg -i", file.toString(), file.toString().replaceAll("mp3", "flac"));
+//        processBuilder.start();
+//        File file2 = new File(file.toString().replaceAll("mp3", "flac"));
+//        AudioSource source = new AudioSource.AudioFileSource(file2);
+//        Fingerprinter fingerprinter = new Fingerprinter();
+//        Fingerprint fingerprint = fingerprinter.apply(source).unsafeRunSync();
+//        System.out.println(fingerprint.compressed());
+//        file2.delete();
     }
 }
